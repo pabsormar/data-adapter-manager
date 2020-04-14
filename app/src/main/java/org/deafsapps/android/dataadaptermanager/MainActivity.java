f95acb8a89ff38ea3209a5ff8e4a93143b05c951
Pablo L. Sordo Martínez <pablodeafsapps@gmail.com>
2020-04-14 14:17:59 +0200

    Add 'ListView', data source and default adapter

--- app/src/main/java/org/deafsapps/android/dataadaptermanager/MainActivity.java
+++ app/src/main/java/org/deafsapps/android/dataadaptermanager/MainActivity.java
@@ -3,6 +3,11 @@ package org.deafsapps.android.dataadaptermanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // data
+        String[] mdsdStudentArray = { "Danilo", "Emiliano", "Felipe", "Peter", "Kanya", "Lourenço", "Nadine", "Paul", "Rainer", "Rodrigo", "Daria", "Pablo" };
+        final List<String> mdsdStudentList = Arrays.asList(mdsdStudentArray);
+        // widget
+        final ListView lvStudents = findViewById(R.id.activity_main__lv__students);
+        // adapter
+        final ArrayAdapter<String> defaultAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mdsdStudentList);
+        // Here is where data and widget are connected through the data adapter
+        lvStudents.setAdapter(defaultAdapter);
     }
 
 }
