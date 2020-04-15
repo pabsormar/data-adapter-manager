6b63312f27ca12d2ebc63a17c33c346d3ed756b9
Pablo L. Sordo Martínez <pablodeafsapps@gmail.com>
2020-04-15 13:36:28 +0200

    Add custom 'ArrayAdapter' and row layout

--- app/src/main/java/org/deafsapps/android/dataadaptermanager/view/MainActivity.java
+++ app/src/main/java/org/deafsapps/android/dataadaptermanager/view/MainActivity.java
@@ -1,10 +1,16 @@
-package org.deafsapps.android.dataadaptermanager;
+package org.deafsapps.android.dataadaptermanager.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.deafsapps.android.dataadaptermanager.R;
+import org.deafsapps.android.dataadaptermanager.data.Student;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter<String> defaultAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mdsdStudentList);
         // Here is where data and widget are connected through the data adapter
         lvStudents.setAdapter(defaultAdapter);
+        lvStudents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked position: " + position + ", id: " + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+        lvStudents.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Long clicked position: " + position + ", id: " + id, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 
 }
