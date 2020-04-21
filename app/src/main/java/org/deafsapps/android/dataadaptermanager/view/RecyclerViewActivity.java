93a4c57a232b6f5134ec68ff61c51fd05517843e
Pablo L. Sordo Martínez <pablodeafsapps@gmail.com>
2020-04-21 13:27:53 +0200

    Update 'RecyclerView' Activity with proper functional implementation

--- app/src/main/java/org/deafsapps/android/dataadaptermanager/view/RecyclerViewActivity.java
+++ app/src/main/java/org/deafsapps/android/dataadaptermanager/view/RecyclerViewActivity.java
@@ -1,11 +1,15 @@
 package org.deafsapps.android.dataadaptermanager.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
 import org.deafsapps.android.dataadaptermanager.R;
+import org.deafsapps.android.dataadaptermanager.adapter.CustomRecyclerViewAdapter;
 import org.deafsapps.android.dataadaptermanager.data.Student;
 
 import java.util.Arrays;
@@ -29,10 +33,13 @@ public class RecyclerViewActivity extends AppCompatActivity {
         // widget
         final RecyclerView rvStudents = findViewById(R.id.activity_recycler_view__rv__student_list);
         // adapter
-
+        final CustomRecyclerViewAdapter adapter = new CustomRecyclerViewAdapter(this, R.layout.row_student, mdsdStudentList);
         // here is where data and widget are connected through the data adapter
-//        rvStudents.setLayoutManager();
-//        rvStudents.setAdapter();
+//        rvStudents.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//        rvStudents.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
+//        rvStudents.setLayoutManager(new GridLayoutManager(this, 3, LinearLayoutManager.HORIZONTAL, false));
+        rvStudents.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.HORIZONTAL));
+        rvStudents.setAdapter(adapter);
     }
 
 }
